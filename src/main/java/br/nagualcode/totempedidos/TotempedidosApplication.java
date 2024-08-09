3c445dd2704e5482e490ceb20652fe78dd0ea37b
Fred <nagual@nagual.local>
2024-08-09 13:11:32 -0300

    auto

---
+++ src/main/java/br/nagualcode/totempedidos/TotempedidosApplication.java
@@ -0,0 +1,13 @@
+package br.nagualcode.totempedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotempedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotempedidosApplication.class, args);
+	}
+
+}
